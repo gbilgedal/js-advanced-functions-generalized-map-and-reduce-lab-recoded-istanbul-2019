9750102a48c98b2a90470abfd45596a2cdc33684
G Bilge Dal <lagala999@gmail.com>
2020-04-30 12:58:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function map(sourceArray, callback){
 let array = [];
   for(let i =0;i<sourceArray.length;i++){
-    array.push(callback(sourceArray));
+    array.push(callback(sourceArray[i]));
   }
 return array;
 }
