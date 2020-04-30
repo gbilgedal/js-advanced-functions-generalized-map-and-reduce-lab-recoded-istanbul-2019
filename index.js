6a8ae9b5685cb2d344ea2b055407b005a1715d16
G Bilge Dal <lagala999@gmail.com>
2020-04-30 12:49:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+function map(sourceArray, callback){
+  return callback(sourceArray);
+}
+function reduce(sourceArray, starting){
+  let total;
+  if(starting)
+    total += starting;
+  sourceArray.forEach(e=>{
+    total+=e;
+  });
+  return total;
+}
